113fe484dd1b418844c9ab8d49ee89cf0eafa466
Aditi <aditiag1402@gmail.com>
2023-09-18 22:04:35 +0530

    Added the functionality to track current high score

--- simon.js
+++ simon.js
@@ -6,7 +6,10 @@ let btns = ["yellow", "red", "purple", "green"];
 let started = false;
 let level = 0;
 
+let high_Score = [0];
+
 let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
 
 document.addEventListener("keypress", function(){
     if(started == false){
@@ -28,6 +31,7 @@ function levelUp(){
     userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
+    h3.innerText = ``;
 
     let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
@@ -46,6 +50,8 @@ function checkAns(idx){
     }
     else{
         h2.innerHTML = `Game Over. Your score was <b>${level}</b><br> Press any key to start.`; 
+        high_Score.push(level -1);
+        h3.innerText = `Current high score: ${Math.max(...high_Score)};`
         document.querySelector("body").style.backgroundColor = "red";
         setTimeout(function(){
             document.querySelector("body").style.backgroundColor = "white";
